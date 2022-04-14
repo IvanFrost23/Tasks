29627d5964fb0478c65acb11e5ecaa19b811b3f3
Ivan Morozov <morozov.medo@gmail.com>
2022-04-15 00:47:01 +0500

    New solution

---
+++ acmp/acmp21.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+public class Main {
+    static int max(int N, int M, int K) {
+        if (N >= M && N >= K)
+            return N;
+        if (M >= N && M >= K)
+            return M;
+        return K;
+    }
+ 
+    static int min(int N, int M, int K) {
+        if (N <= M && N <= K)
+            return N;
+        if (M <= N && M <= K)
+            return M;
+        return K;
+    }
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int K = scanner.nextInt();
+ 
+        System.out.print(max(N, M, K) - min(N, M, K));
+    }
+}
